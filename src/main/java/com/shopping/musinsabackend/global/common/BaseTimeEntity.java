77b7813a3bf347811161eeae3781d7621c378865
wjdah <wjdah1028@naver.com>
2026-02-05 10:11:08 +0900

    :sparkles:Feat:global 패키지 초기 세팅

---
+++ src/main/java/com/shopping/musinsabackend/global/common/BaseTimeEntity.java
@@ -0,0 +1,27 @@
+package com.shopping.musinsabackend.global.common;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@SuperBuilder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseTimeEntity {
+
+    @CreatedDate
+    private LocalDateTime createdAt; // 생성 시간 자동 기록
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt; // 수정 시간 자동 기록
+}
