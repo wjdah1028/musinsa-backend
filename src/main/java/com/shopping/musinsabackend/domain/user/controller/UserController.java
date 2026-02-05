4b0fbd97f00e5641a597229c688ca5f739c0669d
wjdah <wjdah1028@naver.com>
2026-02-05 17:02:44 +0900

    :sparkles:Feat:회원 탈퇴 기능 생성

--- src/main/java/com/shopping/musinsabackend/domain/user/controller/UserController.java
+++ src/main/java/com/shopping/musinsabackend/domain/user/controller/UserController.java
@@ -4,15 +4,14 @@ import com.shopping.musinsabackend.domain.user.dto.request.SignUpRequest;
 import com.shopping.musinsabackend.domain.user.dto.response.SignUpResponse;
 import com.shopping.musinsabackend.domain.user.service.UserService;
 import com.shopping.musinsabackend.global.response.BaseResponse;
+import com.shopping.musinsabackend.global.security.CustomUserDetails;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -23,7 +22,7 @@ public class UserController {
     private final UserService userService;
 
     // 회원 가입 API
-    @Operation(summary = "회원가입", description = "사용자 회원가입 API.")
+    @Operation(summary = "회원가입", description = "사용자 회원가입 API")
     @PostMapping("/sign-up")
     public ResponseEntity<BaseResponse<SignUpResponse>> signUp(@RequestBody @Valid SignUpRequest request) {
 
@@ -32,6 +31,18 @@ public class UserController {
 
         // 응답 반환
         return ResponseEntity.ok(BaseResponse.success(201, "회원가입에 성공했습니다.", signUpResponse));
+    }
+
+    // 회원 탈퇴 API
+    @Operation(summary = "회원탈퇴", description = "사용자 탈퇴 API")
+    @DeleteMapping("/delete-user")
+    public ResponseEntity<BaseResponse<Void>> deleteUser(@AuthenticationPrincipal CustomUserDetails userDetails) {
+
+        // 서비스 호출
+        userService.deleteUser(userDetails.getUsername());
+
+        // 응답 반환
+        return ResponseEntity.ok(BaseResponse.success(200, "회원 탈퇴가 완료되었습니다.", null));
     }
 }
 
