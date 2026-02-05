4f04fcb54cbe01f336036b3dbedf6269679472f0
wjdah <wjdah1028@naver.com>
2026-02-05 11:31:28 +0900

    :sparkles:Feat:프로젝트 재생성으로 인한 파일 재생성

---
+++ src/main/java/com/shopping/musinsabackend/domain/user/controller/UserController.java
@@ -0,0 +1,37 @@
+package com.shopping.musinsabackend.domain.user.controller;
+
+import com.shopping.musinsabackend.domain.user.dto.request.SignUpRequest;
+import com.shopping.musinsabackend.domain.user.dto.response.SignUpResponse;
+import com.shopping.musinsabackend.domain.user.service.UserService;
+import com.shopping.musinsabackend.global.response.BaseResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/users")
+@Tag(name = "User", description = "User 관련 API")
+public class UserController {
+
+    private final UserService userService;
+
+    // 회원 가입 API
+    @Operation(summary = "회원가입", description = "사용자 회원가입 API.")
+    @PostMapping("/sign-up")
+    public ResponseEntity<BaseResponse<SignUpResponse>> signUp(@RequestBody @Valid SignUpRequest request) {
+
+        // 서비스 호출
+        SignUpResponse signUpResponse = userService.signUp(request);
+
+        // 응답 반환
+        return ResponseEntity.ok(BaseResponse.success(201, "회원가입에 성공했습니다.", signUpResponse));
+    }
+}
+
