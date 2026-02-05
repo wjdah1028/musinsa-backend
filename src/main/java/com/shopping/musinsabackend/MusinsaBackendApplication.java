edfb5c788433e10d827da98c26c26df402d73ae5
wjdah <wjdah1028@naver.com>
2026-02-05 09:08:18 +0900

    :sparkles:Feat:프로젝트 생성

---
+++ src/main/java/com/shopping/musinsabackend/MusinsaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.shopping.musinsabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusinsaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusinsaBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shopping/musinsabackend/MusinsaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopping.musinsabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusinsaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
