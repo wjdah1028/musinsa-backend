7ad0c02a0754880db28a42806b295335c8df78d4
wjdah <wjdah1028@naver.com>
2026-02-09 08:56:53 +0900

    :sparkles:Feat:카테고리 Repository 생성

---
+++ src/main/java/com/shopping/musinsabackend/domain/product/repository/CategoryRepository.java
@@ -0,0 +1,8 @@
+package com.shopping.musinsabackend.domain.product.repository;
+
+import com.shopping.musinsabackend.domain.product.entity.CategoryEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<CategoryEntity, Long> {
+
+}
