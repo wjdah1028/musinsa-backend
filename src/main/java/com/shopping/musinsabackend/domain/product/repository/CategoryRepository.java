f6009b724effb1ba06288d4f3616f0bb2e015857
wjdah <wjdah1028@naver.com>
2026-02-09 16:17:20 +0900

    :sparkles:Feat:카테고리 이름 찾는 메서드 추가

--- src/main/java/com/shopping/musinsabackend/domain/product/repository/CategoryRepository.java
+++ src/main/java/com/shopping/musinsabackend/domain/product/repository/CategoryRepository.java
@@ -4,5 +4,5 @@ import com.shopping.musinsabackend.domain.product.entity.CategoryEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CategoryRepository extends JpaRepository<CategoryEntity, Long> {
-
+    boolean existsByCategoryName(String categoryName);
 }
