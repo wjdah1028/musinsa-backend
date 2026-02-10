ea801085aa80402f8f97ad7feda16cd739fcb91b
wjdah <wjdah1028@naver.com>
2026-02-10 17:28:00 +0900

    :sparkles:Feat:S3 에러 코드 추가

--- src/main/java/com/shopping/musinsabackend/global/s3/exception/S3ErrorCode.java
+++ src/main/java/com/shopping/musinsabackend/global/s3/exception/S3ErrorCode.java
@@ -13,7 +13,8 @@ public enum S3ErrorCode implements BaseErrorCode {
     FILE_SIZE_INVALID("IMG4002", "파일 크기는 5MB를 초과할 수 없습니다.", HttpStatus.BAD_REQUEST),
     FILE_TYPE_INVALID("IMG4003", "이미지 파일만 업로드 가능합니다.", HttpStatus.BAD_REQUEST),
     FILE_SERVER_ERROR("IMG5001", "이미지 처리 중 서버 에러, 관리자에게 문의 바랍니다.", HttpStatus.INTERNAL_SERVER_ERROR),
-    INVALID_BASE64("IMG4005", "잘못된 Base64값 입니다.", HttpStatus.BAD_REQUEST);
+    INVALID_BASE64("IMG4005", "잘못된 Base64값 입니다.", HttpStatus.BAD_REQUEST),
+    NO_IMAGE_EXIST("IMG4004", "등록된 이미지가 없습니다.", HttpStatus.NOT_FOUND);
 
     private final String code;
     private final String message;
