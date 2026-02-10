83f559540e18cf2ad5a8dde1e4590863e85ccc62
wjdah <wjdah1028@naver.com>
2026-02-10 11:48:45 +0900

    :sparkles:Feat:상품 조회 기능 생성

--- src/main/java/com/shopping/musinsabackend/domain/product/controller/ProductController.java
+++ src/main/java/com/shopping/musinsabackend/domain/product/controller/ProductController.java
@@ -2,16 +2,21 @@ package com.shopping.musinsabackend.domain.product.controller;
 
 import com.shopping.musinsabackend.domain.product.dto.request.ProductCreateRequest;
 import com.shopping.musinsabackend.domain.product.dto.response.ProductCreateResponse;
+import com.shopping.musinsabackend.domain.product.dto.response.ProductReadResponse;
 import com.shopping.musinsabackend.domain.product.service.ProductService;
 import com.shopping.musinsabackend.global.response.BaseResponse;
+import com.shopping.musinsabackend.global.security.CustomUserDetails;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/products")
@@ -33,5 +38,27 @@ public class ProductController {
 
         // 응답 반환
         return ResponseEntity.ok(BaseResponse.success(200, "상품 등록 성공", response));
+    }
+
+    @Operation(summary = "상품 전체 조회 API")
+    @GetMapping("/all-product")
+    public ResponseEntity<BaseResponse<List<ProductReadResponse>>> getProductList() {
+
+        // 서비스 호출
+        List<ProductReadResponse> response = productService.allProductList();
+
+        // 응답 반환
+        return ResponseEntity.ok(BaseResponse.success(200, "상품 조회 성공", response));
+    }
+
+    @Operation(summary = "상품 상세 조회 API")
+    @GetMapping("/detail-product/{productId}")
+    public ResponseEntity<BaseResponse<ProductReadResponse>> detailProduct(@PathVariable Long productId) {
+
+        // 서비스 호출
+        ProductReadResponse response = productService.productInfo(productId);
+
+        // 응답 반환
+        return ResponseEntity.ok(BaseResponse.success(200, "상품 상세 조회 성공", response));
     }
 }
