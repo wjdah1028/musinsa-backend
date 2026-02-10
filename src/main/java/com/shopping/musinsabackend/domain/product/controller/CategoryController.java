c17e92085ccf0c0b0a14fe9afa966cf8a8e7b31a
wjdah <wjdah1028@naver.com>
2026-02-10 10:49:44 +0900

    :sparkles:Feat:카테고리 조회 기능 생성

--- src/main/java/com/shopping/musinsabackend/domain/product/controller/CategoryController.java
+++ src/main/java/com/shopping/musinsabackend/domain/product/controller/CategoryController.java
@@ -2,6 +2,7 @@ package com.shopping.musinsabackend.domain.product.controller;
 
 import com.shopping.musinsabackend.domain.product.dto.request.CategoryCreateRequest;
 import com.shopping.musinsabackend.domain.product.dto.response.CategoryCreateResponse;
+import com.shopping.musinsabackend.domain.product.dto.response.CategoryReadResponse;
 import com.shopping.musinsabackend.domain.product.service.CategoryService;
 import com.shopping.musinsabackend.global.response.BaseResponse;
 import io.swagger.v3.oas.annotations.Operation;
@@ -9,10 +10,9 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/categories")
@@ -31,5 +31,13 @@ public class CategoryController {
 
         // 응답 반환
         return ResponseEntity.ok(BaseResponse.success(200, "카테고리 등록 성공", categoryCreateResponse));
+    }
+
+    // 2. 카테고리 전체 조회 (이게 없어서 조회를 못하셨던 겁니다!)
+    @Operation(summary = "카테고리 전체 조회")
+    @GetMapping // GET 요청을 받습니다.
+    public ResponseEntity<List<CategoryReadResponse>> getCategoryList() {
+        List<CategoryReadResponse> list = categoryService.getCategoryList();
+        return ResponseEntity.ok(list);
     }
 }
