81969d3b2f4e3c7d6e1f264236d2767120713220
wjdah <wjdah1028@naver.com>
2026-02-10 17:26:56 +0900

    :sparkles:Feat:상품 에러 코드 추가

--- src/main/java/com/shopping/musinsabackend/domain/product/exception/ProductErrorCode.java
+++ src/main/java/com/shopping/musinsabackend/domain/product/exception/ProductErrorCode.java
@@ -12,6 +12,7 @@ public enum ProductErrorCode implements BaseErrorCode {
     // 상품 관련 에러
     PRODUCT_NOT_FOUND("PROD4001", "존재하지 않는 상품입니다.", HttpStatus.NOT_FOUND),
     PRODUCT_ALREADY_EXIST("PROD4006", "이미 존재하는 상품입니다.",  HttpStatus.CONFLICT),
+    PRODUCT_ALL_EXIST("PRO4007", "상품이 존재하지 않습니다.", HttpStatus.NOT_FOUND),
 
     // 브랜드 관련 에러
     BRAND_NOT_FOUND("PROD4002", "존재하지 않는 브랜드입니다.", HttpStatus.BAD_REQUEST),
