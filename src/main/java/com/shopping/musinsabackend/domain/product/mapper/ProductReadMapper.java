d6d0b358a27cc38f3380eef9f9a9550f2bc4cbb2
wjdah <wjdah1028@naver.com>
2026-02-11 12:29:50 +0900

    :memo:Docs:상품 url 추가

--- src/main/java/com/shopping/musinsabackend/domain/product/mapper/ProductReadMapper.java
+++ src/main/java/com/shopping/musinsabackend/domain/product/mapper/ProductReadMapper.java
@@ -3,7 +3,10 @@ package com.shopping.musinsabackend.domain.product.mapper;
 import com.shopping.musinsabackend.domain.product.dto.response.ProductReadResponse;
 import com.shopping.musinsabackend.domain.product.entity.CategoryEntity;
 import com.shopping.musinsabackend.domain.product.entity.ProductEntity;
+import com.shopping.musinsabackend.domain.product.entity.ProductImageEntity; // 추가
 import org.springframework.stereotype.Component;
+
+import java.util.stream.Collectors; // 추가
 
 @Component
 public class ProductReadMapper {
@@ -16,16 +19,19 @@ public class ProductReadMapper {
                 .price(product.getPrice())
                 .brandName(product.getBrand().getBrandName())
                 .categoryName(getCategoryPath(product.getCategory()))
+                .imageUrls(product.getImages().stream()
+                        .map(ProductImageEntity::getImageUrl)
+                        .collect(Collectors.toList()))
                 .build();
     }
 
     // 카테고리 경로
     private String getCategoryPath(CategoryEntity category) {
-        
+
         // 현재 카테고리 출력
         String path = category.getCategoryName();
         CategoryEntity parent = category.getParent();
-        
+
         // 부모가 null일때까지 위로 올라가서 출력
         while (parent != null) {
             path = parent.getCategoryName() + " > " + path;
