a0c0ad13814983278f94fe00df6528d08dea4cfa
wjdah <wjdah1028@naver.com>
2026-02-11 16:59:42 +0900

    :sparkles:Feat:장바구니 특정 상품 확인하는 repository 파일 생성

---
+++ src/main/java/com/shopping/musinsabackend/domain/cart/repository/CartItemRepository.java
@@ -0,0 +1,14 @@
+package com.shopping.musinsabackend.domain.cart.repository;
+
+import com.shopping.musinsabackend.domain.cart.entity.CartEntity;
+import com.shopping.musinsabackend.domain.cart.entity.CartItemEntity;
+import com.shopping.musinsabackend.domain.product.entity.ProductEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface CartItemRepository extends JpaRepository<CartItemEntity, Long> {
+
+    // 특정 장바구니에 특정 상품이 담겨 있는지 확인
+    Optional<CartItemEntity> findByCartAndProduct(CartEntity cart, ProductEntity product);
+}
