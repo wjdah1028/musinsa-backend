f40b32dcd99eff5f7ad66373b28b52cc2ac10eb5
wjdah <wjdah1028@naver.com>
2026-02-12 10:48:26 +0900

    :memo:Docs:사용자와 장바구니 일치 확인 에러코드 추가

--- src/main/java/com/shopping/musinsabackend/domain/cart/exception/CartErrorCode.java
+++ src/main/java/com/shopping/musinsabackend/domain/cart/exception/CartErrorCode.java
@@ -11,7 +11,8 @@ public enum CartErrorCode implements BaseErrorCode {
     CART_NOT_FOUND(HttpStatus.NOT_FOUND, "장바구니를 찾을 수 없습니다."),
     INVALID_QUANTITY(HttpStatus.BAD_REQUEST, "수량은 1개 이상이어야 합니다."),
     ITEM_NOT_FOUND(HttpStatus.NOT_FOUND, "장바구니에 해당 상품이 없습니다."),
-    EMPTY_CART(HttpStatus.NOT_FOUND, "장바구니에 담은 상품이 없습니다. "); // 장바구니 전체 조회할 때 사용
+    EMPTY_CART(HttpStatus.NOT_FOUND, "장바구니에 담은 상품이 없습니다. "), // 장바구니 전체 조회할 때 사용
+    INVALID_USER(HttpStatus.BAD_REQUEST, "사용자의 장바구니와 일치하지 않습니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
