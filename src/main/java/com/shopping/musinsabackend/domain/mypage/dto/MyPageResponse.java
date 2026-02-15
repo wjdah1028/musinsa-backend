238de3297b3890a3157c73e40b7a7e3d98ac1d9b
wjdah <wjdah1028@naver.com>
2026-02-15 20:45:03 +0900

    :sparkles:Feat:마이페이지 응답 dto 생성

---
+++ src/main/java/com/shopping/musinsabackend/domain/mypage/dto/MyPageResponse.java
@@ -0,0 +1,34 @@
+package com.shopping.musinsabackend.domain.mypage.dto;
+
+import com.shopping.musinsabackend.domain.order.dto.response.OrderPastResponse;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Schema(title = "마이페이지 응답 DTO")
+public class MyPageResponse {
+
+    @Schema(description = "회원 이메일", example = "wjdah1028@skuniv.ac.kr")
+    private String email;
+
+    @Schema(description = "회원 이름", example = "홍길동")
+    private String name;
+
+    @Schema(description = "총 주문 횟수", example = "2")
+    private long totalOrder;
+
+    @Schema(description = "진행중인 주문", example = "1")
+    private long activeOrder;
+
+    @Schema(description = "취소된 주문", example = "1")
+    private long cancelOrder;
+
+    @Schema(description = "최근 주문 내역")
+    private List<OrderPastResponse> recentOrder;
+}
